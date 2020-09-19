073b6ad7fc7ae7b83996affc5ad60331f1da1d53
Premwut Klaychim <59070104@it.kmitl.ac.th>
2020-09-20 00:01:32 +0700

    Add User's Google ID when create

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer.js';
+import streamReducer from './streamReducer';
 
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
